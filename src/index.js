caa90cbc1bdb005e32f1792b35b71d7aedf4ea1e
Taiki Noda <84260062+taiki-nd@users.noreply.github.com>
2021-10-16 17:42:35 +0900

    タスク完了処理

--- src/index.js
+++ src/index.js
@@ -15,7 +15,34 @@ const onClickAdd = () => {
   const completeBtn = document.createElement("button");
   completeBtn.innerText = "完了";
   completeBtn.addEventListener("click", () => {
-    alert("完了");
+    // 完了で親タグのdivを削除
+    deleteFromIncompleteLists(completeBtn.parentNode);
+
+    // 完了リストに追加する要素
+    const addTarget = completeBtn.parentNode;
+
+    // todo内容取得
+    const text = addTarget.firstElementChild.innerText;
+
+    // div以下を初期化
+    addTarget.textContent = null;
+
+    // liの生成
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    // 戻すbtn
+    const backBtn = document.createElement("button");
+    backBtn.innerText = "戻す";
+    backBtn.addEventListener("click", () => {
+      
+
+    // divタグの子要素にする設定
+    div.appendChild(li);
+    div.appendChild(backBtn);
+
+    // ulタグの子要素にする設定
+    document.getElementById("complete_lists").appendChild(addTarget);
   });
 
   // btn削除
@@ -23,8 +50,7 @@ const onClickAdd = () => {
   deleteBtn.innerText = "削除";
   deleteBtn.addEventListener("click", () => {
     // 削除で親タグのdivを削除
-    const deleteTarget = deleteBtn.parentNode;
-    document.getElementById("incomplete_lists").removeChild(deleteTarget);
+    deleteFromIncompleteLists(deleteBtn.parentNode);
   });
 
   // divタグの子要素にする設定
@@ -34,6 +60,11 @@ const onClickAdd = () => {
 
   // ulタグの子要素にする設定
   document.getElementById("incomplete_lists").appendChild(div);
+};
+
+// 未完了リストから指定要素の削除
+const deleteFromIncompleteLists = (target) => {
+  document.getElementById("incomplete_lists").removeChild(target);
 };
 
 document
